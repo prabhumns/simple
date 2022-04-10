f08324dbf5f07d9a96cb9baafaea0b0295d34bd2
Prabhu Madipalli <prabhumns123@gmail.com>
2022-04-10 17:25:47 +0530

    First Commit

---
+++ src/test/java/io/madiaplli/prabhu/simple/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.madiaplli.prabhu.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
